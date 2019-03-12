06d28ffb5814a5d4840120b3277017b9bf5d9548
bsdesire <aluno19130@ipt.pt>
2019-03-12 16:48:56 +0000

    Adicionada mensagem de erro

---
+++ Formulario/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Formulario.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        // GET: Home
+        public ActionResult Index()
+        {
+            // Cria o ViewBag e coloca nele um valor vazio
+            ViewBag.mensagem = "";
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Index(string nome, int? idade)
+        {
+            // Agora vai-se fazer alguma coisa...
+
+            string msg;
+
+            if(idade >= 0 && idade <= 130)
+            {
+                // Cria a mensagem a entregar na View
+                msg = "Olá sou o(a) " + nome + " e tenho " + idade + " anos";
+            }
+            else
+            {
+                msg = "Escolha uma idade entre 0 e 130 anos.";
+            }
+            
+
+            // 'Envelopa' a mensagem (msg) paa ser enviada para a view
+            ViewBag.mensagem = msg;
+
+            return View();
+        }
+    }
+}
